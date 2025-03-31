5ce0369708c91295cc82e38e81f29329f50df9ff
Ross Hadden <rosshadden@gmail.com>
2025-03-31 14:45:28 -0400

    feat: moved swift files to sane dirs

--- tests/SwiftTests.swift
+++ tests/SwiftTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import Babel
+@testable import Main
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
