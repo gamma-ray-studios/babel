c868d3f8df498921947d8cd35ab48e5ad4e27339
Ross Hadden <rosshadden@gmail.com>
2025-03-31 14:47:56 -0400

    chore: indents

--- Package.swift
+++ Package.swift
@@ -1,39 +1,38 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGame",
-    platforms: [.macOS(.v13)],
+	name: "SwiftGame",
+	platforms: [.macOS(.v13)],
 
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftGame",
-            type: .dynamic,
-            targets: ["SwiftGame"]),
-    ],
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "SwiftGame",
+			type: .dynamic,
+			targets: ["SwiftGame"]),
+	],
 
-    dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
-    ],
+	dependencies: [
+		.package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
+	],
 
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftGame",
-            dependencies: [
-                "SwiftGodot",
-            ],
-            path: "src",
-            swiftSettings: [.unsafeFlags(["-suppress-warnings"])]
-        ),
-        .testTarget(
-            name: "SwiftTests",
-            dependencies: ["SwiftGame"],
-            path: "tests"
-        ),
-    ]
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "SwiftGame",
+			dependencies: [
+				"SwiftGodot",
+			],
+			path: "src",
+			swiftSettings: [.unsafeFlags(["-suppress-warnings"])]
+		),
+		.testTarget(
+			name: "SwiftTests",
+			dependencies: ["SwiftGame"],
+			path: "tests"
+		),
+	]
 )
