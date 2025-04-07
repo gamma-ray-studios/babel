beaa0ad110cc3d34deb94dae62caec4b1c869901
Ross Hadden <rosshadden@gmail.com>
2025-04-07 16:03:54 -0400

    feat: got TypeScript working again

--- typings/src/scenes/main.nodes.gen.d.ts
+++ typings/src/scenes/main.nodes.gen.d.ts
@@ -1,13 +1,36 @@
 declare module "godot" {
     interface SceneNodes {
         "src/scenes/main.tscn": {
+            gd: Node2D<
+                {
+                    child: Node2D<{}>,
+                }
+            >,
             "c#": Node2D<
                 {
                     child: Node2D<{}>,
                 }
             >,
-            swift: ParentSwift<{}>,
-            rust: ParentRust<{}>,
+            swift: ParentSwift<
+                {
+                    child: ChildSwift<{}>,
+                }
+            >,
+            rust: ParentRust<
+                {
+                    child: ChildRust<{}>,
+                }
+            >,
+            nim: ParentNim<
+                {
+                    child: ChildNim<{}>,
+                }
+            >,
+            typescript: Node2D<
+                {
+                    child: Node2D<{}>,
+                }
+            >,
         },
     }
 }
