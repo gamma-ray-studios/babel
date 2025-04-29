524a59b4b9ff460d5d1a32e00491c3bb26c71f3c
Ross Hadden <rosshadden@gmail.com>
2025-04-28 22:18:26 -0400

    refactor: removed pointer

--- src/mods/parent.v
+++ src/mods/parent.v
@@ -6,10 +6,14 @@ import log
 pub struct ParentV {
 	gd.Node2D
 mut:
-	child ?&ChildV
+	child   ?ChildV
 }
 
 fn (mut self ParentV) ready_() {
+	if gd.Engine.get_singleton().is_editor_hint() {
+		return
+	}
+
 	log.info('ready: ${self.get_name()}')
 
 	if mut child := self.get_node_v('child').cast_to_v[ChildV]('ChildV') {
