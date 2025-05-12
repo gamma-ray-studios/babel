4d4f2d3eeb618efddcea857a7f75dbb5eb160f1f
Ross Hadden <rosshadden@gmail.com>
2025-05-12 11:59:29 -0400

    feat: v updates

--- src/mods/parent.v
+++ src/mods/parent.v
@@ -6,15 +6,13 @@ import log
 pub struct ParentV {
 	gd.Node2D
 mut:
-	child   ?ChildV
+	child ?ChildV
 }
 
 fn (mut s ParentV) ready_() {
 	log.info('ready: ${s.get_name()}')
 
-	if mut child := s.get_node_v('child').cast_to_v[ChildV]('ChildV') {
-		s.child = child
-		idk := Idk{'vvvvvv'}
-		child.setup(idk)
-	}
+	mut child := s.get_node_as[ChildV]('child')
+	s.child = child
+	child.setup(Idk{'vvvvvv'})
 }
